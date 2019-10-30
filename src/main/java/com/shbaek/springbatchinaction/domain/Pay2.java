2ffddb027b29731162e2c31711277e7353a8b34f
shbaek <asmita.bella@gmail.com>
2019-10-30 23:24:02 +0900

    8. Spring Batch 가이드 - ItemWriter

--- src/main/java/com/shbaek/springbatchinaction/domain/Pay2.java
+++ src/main/java/com/shbaek/springbatchinaction/domain/Pay2.java
@@ -17,7 +17,7 @@ import java.time.format.DateTimeFormatter;
 @Setter
 @NoArgsConstructor
 @Entity
-public class Pay {
+public class Pay2 {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
 
     @Id
@@ -27,16 +27,17 @@ public class Pay {
     private String txName;
     private LocalDateTime txDateTime;
 
-    public Pay(Long amount, String txName, LocalDateTime txDateTime) {
+    public Pay2(Long amount, String txName, LocalDateTime txDateTime) {
         this.amount = amount;
         this.txName = txName;
-        this.txDateTime = txDateTime;
+        this.txDateTime =txDateTime;
     }
 
-    public Pay(Long id, Long amount, String txName, LocalDateTime txDateTime) {
+    public Pay2(Long id, Long amount, String txName, LocalDateTime txDateTime) {
         this.id = id;
         this.amount = amount;
         this.txName = txName;
         this.txDateTime = txDateTime;
     }
+
 }
